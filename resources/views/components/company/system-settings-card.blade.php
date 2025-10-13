557100b6834778b6f2e7504426b2ceb15b2481ee
Timjini <hatim.jini@gmail.com>
2025-10-14 00:43:03 +0200

    fix: adding currency

--- resources/views/components/company/system-settings-card.blade.php
+++ resources/views/components/company/system-settings-card.blade.php
@@ -41,7 +41,7 @@
             </div>
             <div>
                 <p class="text-sm font-medium text-gray-500">Default Currency</p>
-                <p class="text-sm text-gray-900">{{ $settings->currency->code }}</p>
+                <p class="text-sm text-gray-900">{{ $settings->currency->code ?? 'Please Set Default Currency' }}</p>
             </div>
 
             <div>
